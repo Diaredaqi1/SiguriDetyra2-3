2ce2625c2b9ea81b203ec57c1fefe9673593f60f
Diaredaqi1 <75573960+Diaredaqi1@users.noreply.github.com>
2021-07-24 00:04:24 +0200

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace TcpSocket
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idbox = new System.Windows.Forms.TextBox();
+            this.passbox = new System.Windows.Forms.TextBox();
+            this.loginb = new System.Windows.Forms.Button();
+            this.sig = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(33, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(33, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // idbox
+            // 
+            this.idbox.Location = new System.Drawing.Point(144, 57);
+            this.idbox.Name = "idbox";
+            this.idbox.Size = new System.Drawing.Size(223, 26);
+            this.idbox.TabIndex = 2;
+            this.idbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passbox
+            // 
+            this.passbox.Location = new System.Drawing.Point(144, 100);
+            this.passbox.Name = "passbox";
+            this.passbox.PasswordChar = '*';
+            this.passbox.Size = new System.Drawing.Size(223, 26);
+            this.passbox.TabIndex = 3;
+            // 
+            // loginb
+            // 
+            this.loginb.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.loginb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginb.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.loginb.Location = new System.Drawing.Point(144, 158);
+            this.loginb.Name = "loginb";
+            this.loginb.Size = new System.Drawing.Size(135, 46);
+            this.loginb.TabIndex = 4;
+            this.loginb.Text = "Login";
+            this.loginb.UseVisualStyleBackColor = false;
+            this.loginb.Click += new System.EventHandler(this.loginb_Click);
+            // 
+            // sig
+            // 
+            this.sig.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.sig.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sig.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.sig.Location = new System.Drawing.Point(277, 158);
+            this.sig.Name = "sig";
+            this.sig.Size = new System.Drawing.Size(142, 46);
+            this.sig.TabIndex = 5;
+            this.sig.Text = "Signature\r\n";
+            this.sig.UseVisualStyleBackColor = false;
+            this.sig.Click += new System.EventHandler(this.sig_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(144, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Verify Signature";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 280);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(464, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(464, 430);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sig);
+            this.Controls.Add(this.loginb);
+            this.Controls.Add(this.passbox);
+            this.Controls.Add(this.idbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.Name = "Login";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idbox;
+        private System.Windows.Forms.TextBox passbox;
+        private System.Windows.Forms.Button loginb;
+        private System.Windows.Forms.Button sig;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
