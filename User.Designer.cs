b85d79154150d780d984d21d00139592e623e479
DianaBeqiri <74824755+DianaBeqiri@users.noreply.github.com>
2021-07-24 00:14:00 +0200

    Add files via upload

---
+++ User.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace TcpSocket
+{
+    partial class User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tid = new System.Windows.Forms.TextBox();
+            this.temri = new System.Windows.Forms.TextBox();
+            this.tmbiemri = new System.Windows.Forms.TextBox();
+            this.tpass = new System.Windows.Forms.TextBox();
+            this.tfatura = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tviti = new System.Windows.Forms.TextBox();
+            this.teuro = new System.Windows.Forms.TextBox();
+            this.tmuaji = new System.Windows.Forms.TextBox();
+            this.tdyqani = new System.Windows.Forms.TextBox();
+            this.btnRuaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Lastname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password";
+            // 
+            // tid
+            // 
+            this.tid.Location = new System.Drawing.Point(156, 63);
+            this.tid.Name = "tid";
+            this.tid.Size = new System.Drawing.Size(130, 26);
+            this.tid.TabIndex = 5;
+            // 
+            // temri
+            // 
+            this.temri.Location = new System.Drawing.Point(156, 111);
+            this.temri.Name = "temri";
+            this.temri.Size = new System.Drawing.Size(130, 26);
+            this.temri.TabIndex = 6;
+            // 
+            // tmbiemri
+            // 
+            this.tmbiemri.Location = new System.Drawing.Point(156, 158);
+            this.tmbiemri.Name = "tmbiemri";
+            this.tmbiemri.Size = new System.Drawing.Size(130, 26);
+            this.tmbiemri.TabIndex = 7;
+            // 
+            // tpass
+            // 
+            this.tpass.Location = new System.Drawing.Point(156, 200);
+            this.tpass.Name = "tpass";
+            this.tpass.PasswordChar = '*';
+            this.tpass.Size = new System.Drawing.Size(130, 26);
+            this.tpass.TabIndex = 8;
+            // 
+            // tfatura
+            // 
+            this.tfatura.Location = new System.Drawing.Point(156, 317);
+            this.tfatura.Name = "tfatura";
+            this.tfatura.Size = new System.Drawing.Size(130, 26);
+            this.tfatura.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Algerian", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(127, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Outlays";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Invoice type";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(39, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Year";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(36, 415);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Month";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(35, 461);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "EuroValue";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(39, 510);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 20);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Store";
+            // 
+            // tviti
+            // 
+            this.tviti.Location = new System.Drawing.Point(156, 361);
+            this.tviti.Name = "tviti";
+            this.tviti.Size = new System.Drawing.Size(130, 26);
+            this.tviti.TabIndex = 16;
+            // 
+            // teuro
+            // 
+            this.teuro.Location = new System.Drawing.Point(156, 455);
+            this.teuro.Name = "teuro";
+            this.teuro.Size = new System.Drawing.Size(130, 26);
+            this.teuro.TabIndex = 17;
+            // 
+            // tmuaji
+            // 
+            this.tmuaji.Location = new System.Drawing.Point(156, 409);
+            this.tmuaji.Name = "tmuaji";
+            this.tmuaji.Size = new System.Drawing.Size(130, 26);
+            this.tmuaji.TabIndex = 18;
+            // 
+            // tdyqani
+            // 
+            this.tdyqani.Location = new System.Drawing.Point(156, 504);
+            this.tdyqani.Name = "tdyqani";
+            this.tdyqani.Size = new System.Drawing.Size(130, 26);
+            this.tdyqani.TabIndex = 19;
+            // 
+            // btnRuaj
+            // 
+            this.btnRuaj.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnRuaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRuaj.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnRuaj.Location = new System.Drawing.Point(114, 556);
+            this.btnRuaj.Name = "btnRuaj";
+            this.btnRuaj.Size = new System.Drawing.Size(93, 46);
+            this.btnRuaj.TabIndex = 20;
+            this.btnRuaj.Text = "Save";
+            this.btnRuaj.UseVisualStyleBackColor = false;
+            this.btnRuaj.Click += new System.EventHandler(this.btnRuaj_Click_1);
+            // 
+            // User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(327, 614);
+            this.Controls.Add(this.btnRuaj);
+            this.Controls.Add(this.tdyqani);
+            this.Controls.Add(this.tmuaji);
+            this.Controls.Add(this.teuro);
+            this.Controls.Add(this.tviti);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tfatura);
+            this.Controls.Add(this.tpass);
+            this.Controls.Add(this.tmbiemri);
+            this.Controls.Add(this.temri);
+            this.Controls.Add(this.tid);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "User";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tid;
+        private System.Windows.Forms.TextBox temri;
+        private System.Windows.Forms.TextBox tmbiemri;
+        private System.Windows.Forms.TextBox tpass;
+        private System.Windows.Forms.TextBox tfatura;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tviti;
+        private System.Windows.Forms.TextBox teuro;
+        private System.Windows.Forms.TextBox tmuaji;
+        private System.Windows.Forms.TextBox tdyqani;
+        private System.Windows.Forms.Button btnRuaj;
+    }
+}
