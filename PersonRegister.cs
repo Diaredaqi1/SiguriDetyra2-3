f3d5b2412f7ef7d101ea12abfbfed9677cfc3918
Diaredaqi1 <75573960+Diaredaqi1@users.noreply.github.com>
2021-07-24 00:05:34 +0200

    Add files via upload

---
+++ PersonRegister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpSocket
+{
+    public class PersonRegister
+    {
+        public int id1 { get; set; }
+        public string emri1 { get; set; }
+        public string mbiemri1 { get; set; }
+        public string passwordi1 { get; set; }
+    }
+}
